7c1cb56c927f77f8f74eb56f8307e2e167798bff
Luke Fritz <lukefritz@gmail.com>
2023-01-02 01:52:05 -0600

    Add debugging for TFC token import (test)

--- scripts/terraform/set-tfc-credentials.sh
+++ scripts/terraform/set-tfc-credentials.sh
@@ -6,8 +6,15 @@ function main() {
   # Create the containing directory tree
   mkdir -p "$tfrc_path"
 
+  local -r tfrc_file_path="$tfrc_path/credentials.tfrc.json"
+
   # Save access token to credentials file
-  echo '{}' | jq ".credentials[\"app.terraform.io\"].token |= \"$TFC_ACCESS_TOKEN\"" > "$tfrc_path/credentials.tfrc.json"
+  echo '{}' | jq ".credentials[\"app.terraform.io\"].token |= \"$TFC_ACCESS_TOKEN\"" > "$tfrc_file_path"
+
+  chmod 600 "$tfrc_file_path"
+
+  echo "Terraform Cloud credentials file saved to $tfrc_file_path"
+  ls -la "$tfrc_file_path"
 }
 
 main "$@"
