a3c4b76f4f23467d80e29141ff70b6b089fa0c7e
Luke Fritz <lukefritz@gmail.com>
2023-01-23 23:45:50 -0600

    Use unique GPG key for sops (patch)

--- scripts/utilities/import-gpg-key.sh
+++ scripts/utilities/import-gpg-key.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
 function main() {
-  echo "$CI_BOT_GPG_KEY" | gpg --batch --yes --no-tty --always-trust --import
-  echo "ENCRYPTION_TEST" | gpg --always-trust --encrypt --recipient "$CI_BOT_GPG_KEY_SUBJECT" > "temp.encrypted"
-  gpg --batch --yes --no-tty --decrypt --passphrase "$CI_BOT_GPG_KEY_PASSPHRASE" --pinentry-mode loopback --always-trust "temp.encrypted"
+  echo "$IMPORT_GPG_KEY" | gpg --batch --yes --no-tty --always-trust --import
+  echo "ENCRYPTION_TEST" | gpg --always-trust --encrypt --recipient "$IMPORT_GPG_KEY_SUBJECT" > "temp.encrypted"
+  gpg --batch --yes --no-tty --decrypt --passphrase "$IMPORT_GPG_KEY_PASSPHRASE" --pinentry-mode loopback --always-trust "temp.encrypted"
   rm -f "temp.encrypted";
-  git config --global user.signingkey "$CI_BOT_GPG_KEY_ID"
-  git config --global commit.gpgsign true
+
+  if [[ "$SET_GIT_GPG_CONFIG" == "true" ]]; then
+    git config --global user.signingkey "$IMPORT_GPG_KEY_ID"
+    git config --global commit.gpgsign true
+  fi
 }
 
 main "$@"
