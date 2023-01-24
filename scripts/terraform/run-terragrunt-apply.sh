55da2e3aa36a11c6f62625424ffa164b7a9247ec
Luke Fritz <lukefritz@gmail.com>
2023-01-23 23:28:44 -0600

    Prevent sops from prompting for GPG key passphrase (fix)

--- scripts/terraform/run-terragrunt-apply.sh
+++ scripts/terraform/run-terragrunt-apply.sh
@@ -3,6 +3,11 @@
 function main() {
   local -r base_directory="$1"
 
+  # Run a full encrypt/decrypt lifecycle to prevent sops from prompting for GPG key passphrase
+  echo "ENCRYPTION_TEST" | gpg --always-trust --encrypt --recipient "$CI_BOT_GPG_KEY_SUBJECT" > "temp.encrypted"
+  gpg --batch --yes --no-tty --decrypt --passphrase "$CI_BOT_GPG_KEY_PASSPHRASE" --pinentry-mode loopback --always-trust "temp.encrypted"
+  rm -f "temp.encrypted";
+
   # Initialize Terraform and upgrade provider/module references
   terragrunt run-all init -upgrade \
     --terragrunt-non-interactive \
